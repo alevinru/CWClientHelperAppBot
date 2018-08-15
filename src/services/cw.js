5a21cc8cd3ad8b74b4c68f575f552d602e353f19
Alexander Levin <alevin.ru@gmail.com>
2018-08-15 15:55:51 +0300

    commented out cw fanouts

--- src/services/cw.js
+++ src/services/cw.js
@@ -1,8 +1,17 @@
-import CWExchange from 'cw-rest-api';
+import CWExchange, * as CW from 'cw-rest-api';
 
 const debug = require('debug')('laa:cwb:cw');
 
 export const { CW_BOT_ID } = process.env;
 
+const fanouts = [
+  // CW.QUEUE_DEALS,
+  // CW.QUEUE_AU,
+  // CW.QUEUE_OFFERS,
+  // CW.QUEUE_SEX,
+  // CW.QUEUE_YELLOW_PAGES,
+];
+
+export const cw = new CWExchange({ fanouts });
 
 debug('Started CW API', CW_BOT_ID);

8de94d74609b08ccda45d94b817d4d33f1a7fa97
Alexander Levin <alevin.ru@gmail.com>
2018-08-16 10:57:10 +0300

    hello responds with firstName

--- src/middleware/hello.js
+++ src/middleware/hello.js
@@ -1,12 +1,12 @@
 export default function (ctx) {
 
-  const { session, reply, from: { id: userId } } = ctx;
+  const { session, reply, from: { id: userId, first_name: firstName } } = ctx;
 
   if (!session.auth) {
     reply('We\'re not familiar yet. You should /auth first to start talking.');
     return;
   }
 
-  reply(`Hi there, buddy! Your user id is *${userId}*`, { parse_mode: 'Markdown' });
+  reply(`Hi there, *${firstName}*! Your user id is *${userId}*`, { parse_mode: 'Markdown' });
 
 }

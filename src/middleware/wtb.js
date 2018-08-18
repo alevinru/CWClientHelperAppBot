9872ad675b971ace3c4d4001e0de5a8adaa53616
Alexander Levin <alevin.ru@gmail.com>
2018-08-18 21:01:15 +0300

    wtb with price limit

--- src/middleware/wtb.js
+++ src/middleware/wtb.js
@@ -1,4 +1,5 @@
 import { cw, getAuthToken } from '../services';
+import { checkPrice } from './trades';
 
 const debug = require('debug')('laa:cwb:wtb');
 
@@ -8,21 +9,35 @@ export default async function (ctx) {
     match,
     from: { id: userId },
     session,
-    session: { profile: { userName } },
+    session: { profile },
   } = ctx;
   const [, itemCode, quantity, price] = match;
   const wtb = `/wtb_${itemCode}_${quantity}_${price}`;
 
   debug(wtb);
 
+  if (!profile) {
+    ctx.reply('You are not authorized yet, do /auth prior to trading.');
+    return;
+  }
+
+  const { userName } = profile;
+
   try {
+
     const token = getAuthToken(session);
     const dealParams = { itemCode, quantity, price };
+
+    await checkPrice(itemCode, price);
+
     const deal = await cw.wantToBy(parseInt(userId, 0), dealParams, token);
+
     const { itemName, quantity: dealQuantity } = deal;
     const tried = `I have done ${wtb} for <b>${userName}</b>`;
     const got = `response of <b>${dealQuantity}</b> x <b>${itemName}</b>`;
-    ctx.replyHTML(`${tried} and got ${got}`);
+
+    await ctx.replyHTML(`${tried} and got ${got}`);
+
   } catch (e) {
     ctx.replyError(wtb, e);
   }

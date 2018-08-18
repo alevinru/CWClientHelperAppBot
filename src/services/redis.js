d3a405c284a4fdc9936e042311ded3762ac8904b
Alexander Levin <alevin.ru@gmail.com>
2018-08-18 19:39:42 +0300

    redis service

---
+++ src/services/redis.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+export const client = redis.createClient({
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379,
+  db: process.env.REDIS_DB || 0,
+  enable_offline_queue: false,
+});
+
+export const ltrimAsync = promisify(client.ltrim).bind(client);
+export const lpushAsync = promisify(client.lpush).bind(client);
+export const hgetAsync = promisify(client.hget).bind(client);
+export const hsetAsync = promisify(client.hset).bind(client);
+
+const debug = require('debug')('laa:cwc:redis');
+
+client.on('error', err => {
+  debug('Error', err);
+});
+
+client.on('connect', () => {
+  debug('Redis connected');
+});

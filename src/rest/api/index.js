6f541d47e9f9a0c864d40472121b19cd0b260ebd
Alexander Levin <alevin.ru@gmail.com>
2019-01-25 14:32:25 +0200

    GET /au

--- src/rest/api/index.js
+++ src/rest/api/index.js
@@ -1,6 +1,8 @@
 import Router from 'koa-router';
+import * as CW from 'cw-rest-api';
+
 import log from '../../services/log';
-import { lrangeAsync } from '../../services/redis';
+import { hgetAsync, lrangeAsync } from '../../services/redis';
 import { dealsKey } from '../../consumers/dealsConsumer';
 
 const { debug, error } = log('rest:api');
@@ -20,6 +22,25 @@ router.get('/deals/:itemCode', async ctx => {
     const data = await lrangeAsync(dealsKey(itemCode), 0, -1);
 
     ctx.body = data.map(JSON.parse);
+
+  } catch (err) {
+    ctx.response.status = 500;
+    error(err.name, err.message);
+  }
+
+});
+
+router.get('/au', async ctx => {
+
+  // const { params: { itemCode }, header: { authorization } } = ctx;
+
+  debug('GET /au');
+
+  try {
+
+    const data = await hgetAsync(CW.QUEUE_AU, 'data');
+
+    ctx.body = JSON.parse(data);
 
   } catch (err) {
     ctx.response.status = 500;

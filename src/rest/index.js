d422bfc10b0bc1968b16391a7c054e3cbd486af7
Alexander Levin <alevin.ru@gmail.com>
2019-03-01 14:23:22 +0200

    use cwApi.routes()

--- src/rest/index.js
+++ src/rest/index.js
@@ -1,5 +1,6 @@
 import Koa from 'koa';
 import bodyParser from 'koa-body';
+import cwApi from 'cw-rest-api/lib/api/index';
 import log from '../services/log';
 
 import api from './api';
@@ -19,5 +20,6 @@ debug('starting on port', REST_PORT);
 app
   .use(bodyParser())
   .use(api.routes())
+  .use(cwApi.routes())
   .use(api.allowedMethods())
   .listen(REST_PORT);

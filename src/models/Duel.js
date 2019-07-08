3af86b495a226174d6ea6e3e6dc7e9938fed6718
Alexander Levin <alevin.ru@gmail.com>
2019-07-08 13:55:27 +0300

    compute indexed Duel.players

--- src/models/Duel.js
+++ src/models/Duel.js
@@ -1,10 +1,12 @@
 import { Schema, model } from 'mongoose';
+import pick from 'lodash/fp/pick';
 
 const schema = new Schema({
   winner: Object,
   loser: Object,
   isChallenge: Boolean,
   isGuildDuel: Boolean,
+  players: Array,
   ts: Date,
 }, {
   collection: 'Duel',
@@ -17,4 +19,19 @@ schema.index({ 'loser.name': 1, ts: -1 });
 schema.index({ 'winner.tag': 1, ts: -1 });
 schema.index({ 'loser.tag': 1, ts: -1 });
 
+schema.index({ 'players.tag': 1, ts: -1 });
+schema.index({ 'players.name': 1, ts: -1 });
+schema.index({ 'players.id': 1, ts: -1 });
+
+const playerColumns = pick(['id', 'name', 'tag']);
+
+schema.pre('save', setPlayers);
+
 export default model('Duel', schema);
+
+async function setPlayers() {
+  this.players = [
+    playerColumns(this.winner),
+    playerColumns(this.loser),
+  ];
+}

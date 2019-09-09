59fef45f7d8fd38cad400203b206110fd94d6e36
Alexander Levin <alevin.ru@gmail.com>
2019-09-09 12:56:27 +0300

    Update battleDigests.js

--- src/notify/battleDigests.js
+++ src/notify/battleDigests.js
@@ -4,57 +4,65 @@ import { battleView } from '../middleware/battles';
 import { battleDate } from '../services/battles';
 import Battle from '../models/Battle';
 import Chat from '../models/Chat';
-import bot from '../services/bot';
 import log from '../services/log';
 
 const { debug, error } = log('battleDigests');
 
 export default class BattleDigests {
 
+  constructor({ bot, botId }) {
+    this.bot = bot;
+    this.botId = botId;
+  }
+
   async init() {
 
-    const chats = await chatsToNotify();
+    const chats = await this.chatsToNotify();
 
     debug('init', chats.length);
 
     this.battleWatch = Battle.watch()
       .on('change', ({ operationType, fullDocument }) => {
         debug(operationType);
-        return operationType === 'insert' && onBattle(fullDocument);
+        return operationType === 'insert' && this.onBattle(fullDocument);
       });
 
   }
 
-}
-
-function chatsToNotify() {
-  return Chat.find({ 'setting.notifyBattle': true });
-}
-
-function onBattle(battle) {
-  const now = battleDate(new Date());
-  if (now > battle.date) {
-    debug('ignore', battle.date);
-    return;
+  chatsToNotify() {
+    return Chat.find({ 'setting.notifyBattle': true, botId: this.botId });
   }
-  notifyBattle(battle)
-    .catch(error);
-}
 
-async function notifyBattle(battle) {
-  const msg = battleView(battle).join('\n');
-  const chats = await chatsToNotify();
-  debug('notifyBattle', chats.length);
-  await eachSeriesAsync(chats, async chat => {
-    await notify(chat.id, msg);
-  });
-}
-
-async function notify(userId, msg) {
-  debug('notify', userId, msg.length);
-  const options = { parse_mode: 'HTML', disable_notification: true, disable_web_page_preview: true };
-  return bot.telegram.sendMessage(userId, msg, options)
-    .catch(err => {
-      error('notify:sendMessage', userId, err.message);
+  async notifyBattle(battle) {
+    const msg = battleView(battle).join('\n');
+    const chats = await this.chatsToNotify();
+    debug('notifyBattle', chats.length);
+    await eachSeriesAsync(chats, async chat => {
+      await this.notify(chat.id, msg);
     });
+  }
+
+  onBattle(battle) {
+    const now = battleDate(new Date());
+    if (now > battle.date) {
+      debug('ignore', battle.date);
+      return;
+    }
+    this.notifyBattle(battle)
+      .catch(error);
+  }
+
+  async notify(userId, msg) {
+    debug('notify', userId, msg.length);
+    const options = { parse_mode: 'HTML', disable_notification: true, disable_web_page_preview: true };
+    return this.bot.telegram.sendMessage(userId, msg, options)
+      .catch(async err => {
+        error('notify:sendMessage', userId, err.message);
+        if (/403/.test(err.message)) {
+          await Chat.saveValue(userId, 'notifyBattle', false);
+          error('notify:sendMessage', 'turn off notifyBattle for', userId);
+        }
+      });
+  }
+
 }

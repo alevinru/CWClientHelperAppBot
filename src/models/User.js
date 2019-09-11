dc2f0cdf69516d5f7339a92be4135c8c4059673b
Alexander Levin <alevin.ru@gmail.com>
2019-09-11 12:20:01 +0300

    User.settings by bot

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,8 @@
 import { Schema, model } from 'mongoose';
 
+const { BOT_TOKEN } = process.env;
+export const BOT_ID = parseInt(BOT_TOKEN.match(/^[^:]*/)[0], 0);
+
 const schema = new Schema({
   id: String,
   firstName: String,
@@ -9,9 +12,11 @@ const schema = new Schema({
   gear: Object,
   trusts: Object,
   ts: Date,
-  settings: {
-    type: Object,
-    default: {},
+  botSettings: {
+    [BOT_ID]: {
+      type: Object,
+      default: {},
+    },
   },
 }, {
   collection: 'User',
@@ -20,4 +25,25 @@ const schema = new Schema({
 schema.index({ id: 1 });
 schema.index({ guildTag: 1 });
 
+schema.virtual('settings')
+  .get(getSettings)
+  .set(setSettings);
+
 export default model('User', schema);
+
+function setSettings(settings) {
+  if (!this.botSettings) {
+    this.botSettings = {};
+  }
+  this.botSettings[BOT_ID] = settings;
+}
+
+function getSettings() {
+  if (!this.botSettings) {
+    this.botSettings = {};
+  }
+  if (!this.botSettings[BOT_ID]) {
+    this.botSettings[BOT_ID] = {};
+  }
+  return this.botSettings[BOT_ID];
+}

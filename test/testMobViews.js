06e6314070c050d92ec63708ce2df494787972df
Alexander Levin <alevin.ru@gmail.com>
2019-09-30 14:13:58 +0300

    Update testMobViews.js

--- test/testMobViews.js
+++ test/testMobViews.js
@@ -21,4 +21,21 @@ describe('Met some mob parser', function () {
 
   });
 
+  it('should parse EN multi text', async function () {
+
+    const text = await readFile('static/mobsMet_en.txt');
+    assert(text, 'No sample text found');
+
+    const mobs = mobsFromText(text.toString());
+
+    const view = mobOfferView({ ...mobs, date: new Date() });
+
+    const lines = view.text.split('\n');
+
+    expect(lines[0]).to.equal('👾 fight in <b>03:00</b>');
+    expect(lines[1]).to.equal('');
+    expect(lines[2]).to.equal('<code>68</code> Forbidden Knight 🛡');
+
+  });
+
 });
